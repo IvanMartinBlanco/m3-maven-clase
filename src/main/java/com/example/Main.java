6492c8d20d4cd122439adf199793892ae6805a2d
Ivan <ivanmartinblanco1985@gmail.com>
2021-07-22 10:57:21 +0200

    Commit inicio

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Calculator calculator = new Calculator();
+		calculator.sum(2, 2);
+System.out.println(calculator.sum(2, 2));
+	}
+
+}
